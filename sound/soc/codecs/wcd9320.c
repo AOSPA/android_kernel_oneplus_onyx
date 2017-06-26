ae22a946b324f6cfcbc6a6fbd5e31314046b39f4
Ícaro Hoff <icarohoff@gmail.com>
2017-06-26 06:40:12 -0600

    ASoC: wcd9320: enhance sound control
    Thanks to, and authored as Ícaro Hoff
    
    Changes:
    *Support for PA gain
    *Support for proper mic gain
    *Squashed enhancement of the changes made to hammerhead
    
    Additions:
    *LGE and STWeaks related controls were removed
    *Left and right headphone gain were enabled
    *Cache values are now written to
    
    /* TODO */
    Ensure that writing to the enabled cache values works
    Adapt for hpm
    
    Change-Id: I0ac65ba9894850cb9d465791c901614cc71955e9

--- sound/soc/codecs/wcd9320.c
+++ sound/soc/codecs/wcd9320.c
@@ -4792,9 +4792,9 @@ static int taiko_volatile(struct snd_soc_codec *ssc, unsigned int reg)
 	if (reg == TAIKO_A_RX_HPH_CNP_EN)
 		return 1;
 
-#if defined(CONFIG_SOUND_CONTROL_HAX_3_GPL) && defined(CONFIG_MACH_LGE)
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
 	/* HPH gain registers */
-	if (lge_snd_pa_ctrl_locked) {
+	if (snd_pa_ctrl_locked) {
 		if (reg == TAIKO_A_RX_HPH_L_GAIN ||
 				reg == TAIKO_A_RX_HPH_R_GAIN)
 			return 1;
@@ -7698,7 +7698,6 @@ static int taiko_codec_probe(struct snd_soc_codec *codec)
 	struct wcd9xxx_core_resource *core_res;
 
 #ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
-	pr_info("probing taiko codec...\n");
 	fauxsound_codec_ptr = codec;
 #endif
 	codec->control_data = dev_get_drvdata(codec->dev->parent);
