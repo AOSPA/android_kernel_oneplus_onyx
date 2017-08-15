35849de972edf944f2b889c5192186f876efde78
CertifiedBlyndGuy <rjmurdok@linux.com>
2017-08-15 17:34:57 -0600

    input: touchscreen: s3508: Fire keys on gesture deployment
    Change-Id: Id74a1621e3df39e8604565fb9cb201a624556edb

--- drivers/input/touchscreen/synaptics_driver_s3508.c
+++ drivers/input/touchscreen/synaptics_driver_s3508.c
@@ -128,28 +128,19 @@ struct test_header {
 #define Mgesture            12  // M
 #define Wgesture            13  // W
 
-#define KEY_DOUBLE_TAP          249 // double tap
-#define KEY_GESTURE_CIRCLE      250 // draw circle
-#define KEY_GESTURE_TWO_SWIPE   251 // swipe two finger vertically
-#define KEY_GESTURE_DOWN_ARROW  252 // draw down arrow
-#define KEY_GESTURE_LEFT_ARROW  253 // draw left arrow
-#define KEY_GESTURE_RIGHT_ARROW 254 // draw right arrow
-#define KEY_GESTURE_UP_ARROW    255 // draw up arrow
-#define KEY_GESTURE_LETTER_W    256 // draw letter "W"
-#define KEY_GESTURE_LETTER_M    257 // draw letter "M"
-#define KEY_GESTURE_LTR_SWIPE   258 // swipe left to right
-#define KEY_GESTURE_RTL_SWIPE   259 // swipe right to left
-#define KEY_GESTURE_UTD_SWIPE   260 // swipe up to down
-#define KEY_GESTURE_DTU_SWIPE   261 // swipe down to up
-
-#define BIT0 (0x1 << 0)
-#define BIT1 (0x1 << 1)
-#define BIT2 (0x1 << 2)
-#define BIT3 (0x1 << 3)
-#define BIT4 (0x1 << 4)
-#define BIT5 (0x1 << 5)
-#define BIT6 (0x1 << 6)
-#define BIT7 (0x1 << 7)
+#define KEY_DOUBLE_TAP          KEY_F1	// double tap to wake
+#define KEY_GESTURE_CIRCLE      KEY_F2	// draw circle to lunch camera
+#define KEY_GESTURE_TWO_SWIPE	KEY_F3	// swipe two finger vertically to play/pause
+#define KEY_GESTURE_V           KEY_F4	// draw v to toggle flashlight
+#define KEY_GESTURE_LEFT_V      KEY_F5	// draw left arrow for previous track
+#define KEY_GESTURE_RIGHT_V     KEY_F6	// draw right arrow for next track
+#define KEY_GESTURE_INVERT_V    KEY_F7
+#define KEY_GESTURE_SWIPE_LEFT  KEY_F8
+#define KEY_GESTURE_SWIPE_RIGHT KEY_F9
+#define KEY_GESTURE_M           KEY_F10
+#define KEY_GESTURE_W           KEY_F11
+#define KEY_GESTURE_SWIPE_UP    KEY_F12
+#define KEY_GESTURE_SWIPE_DOWN  KEY_F13
 #endif
 
 /*********************for Debug LOG switch*******************/
@@ -3486,7 +3477,7 @@ static int	synaptics_input_init(struct synaptics_ts_data *ts)
 		TPD_ERR("synaptics_ts_probe: Failed to allocate input device\n");
 		return -1;
 	}
-	ts->input_dev->name = TPD_DEVICE;;
+	ts->input_dev->name = TPD_NAME;;
 	set_bit(EV_SYN, ts->input_dev->evbit);
 	set_bit(EV_ABS, ts->input_dev->evbit);
 	set_bit(EV_KEY, ts->input_dev->evbit);
@@ -3497,7 +3488,6 @@ static int	synaptics_input_init(struct synaptics_ts_data *ts)
 	set_bit(INPUT_PROP_DIRECT, ts->input_dev->propbit);
 
 #ifdef SUPPORT_GESTURE
-	set_bit(KEY_F4 , ts->input_dev->keybit);//doulbe-tap resume
 	set_bit(KEY_DOUBLE_TAP, ts->input_dev->keybit);
 	set_bit(KEY_GESTURE_CIRCLE, ts->input_dev->keybit);
 	set_bit(KEY_GESTURE_UP_ARROW, ts->input_dev->keybit);
